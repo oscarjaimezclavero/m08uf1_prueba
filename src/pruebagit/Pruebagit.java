6f3fd6b34fb8e2620aa8cbbaaf3189eb0674f397
oscar <oscar@DESKTOP-IP5BVTS>
2020-09-23 19:37:55 +0200

    ojc - commit tercero

--- src/pruebagit/Pruebagit.java
+++ src/pruebagit/Pruebagit.java
@@ -19,6 +19,8 @@ public class Pruebagit {
         system.out.print("prueba");
         
         echo "adeu fins demá";
+        
+        System.out.println("a ver si se suma un commitS");
     }
     
 }
